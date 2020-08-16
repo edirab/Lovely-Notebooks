b3078054511f65587109f339723ed854c64eba3e
edirab <edirab@yandex.ru>
2020-08-16 22:19:26 +0300

    Алгоритм Монте-Карло для рассчёта числа Пи

---
+++ stepic-python/debugStuff/v10.py
@@ -0,0 +1,27 @@
+def popular_words(text: str, words: list) -> dict:
+    # your code here
+    return None
+
+
+if __name__ == '__main__':
+    print("Example:")
+    print(popular_words('''
+When I was One
+I had just begun
+When I was Two
+I was nearly new
+''', ['i', 'was', 'three', 'near']))
+
+    # These "asserts" are used for self-checking and not for an auto-testing
+    assert popular_words('''
+When I was One
+I had just begun
+When I was Two
+I was nearly new
+''', ['i', 'was', 'three', 'near']) == {
+        'i': 4,
+        'was': 3,
+        'three': 0,
+        'near': 0
+    }
+    print("Coding complete? Click 'Check' to earn cool rewards!")
